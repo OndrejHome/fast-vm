843af1dc12c999477d49cf954f84b56c96f28c68
Ondrej Famera <ondrej@famera.cz>
2016-01-27 23:45:03 +0100

    make stup.sh executable and add check for commands that are essential for script to work

--- setup.sh
+++ setup.sh
@@ -5,6 +5,18 @@ echo "copying fast-vm into /usr/local/sbin/fast-vm"
 cp fast-vm /usr/local/sbin
 echo "## running configuration scripts for initial setup"
 
+cmds="lvcreate lvconvert gunzip virsh virt-edit"
+
+for i in $cmds
+do
+	which "$i" >/dev/null 2>&1
+	if [ ! "$?" -eq "0" ];
+	then
+		echo "command $i not found, please install it before continuing"
+		exit 1
+	fi
+done
+
 . setup-general.sh
 . setup-thin-lvm.sh
 . setup-libvirt-net.sh
