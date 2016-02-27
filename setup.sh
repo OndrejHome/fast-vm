b6f38cf0da65ad35990c1fce5a16de41b92f4d69
Ondrej Famera <ondrej@famera.cz>
2016-02-27 11:17:09 +0100

    - setup.sh asks for upgrade if the fast-vm script is already present

--- setup.sh
+++ setup.sh
@@ -5,6 +5,15 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 echo "##==>> fast-vm setup script"
+
+if [ -f "/usr/local/sbin/fast-vm" ]; then
+	echo "[??] File /usr/local/sbin/fast-vm already present, do you want to just update fast-vm without running configuration? [yes] "
+	read just_upgrade
+
+	if [ -z "$just_upgrade" ] || [ "$just_updagrade" == "yes" ]; then
+		echo "[info] setup will only update fast-vm scripts and then exit without reconfiguration"
+	fi
+fi
 echo "copying fast-vm into /usr/local/sbin/fast-vm"
 cp fast-vm /usr/local/sbin
 
@@ -12,8 +21,8 @@ bash_completion_dir=$(pkg-config --variable=completionsdir bash-completion 2>/de
 if [ -d "$bash_completion_dir" ]; then
 	cp fast-vm.bash_completion $bash_completion_dir/fast-vm
 fi
-echo "## running configuration scripts for initial setup"
 
+echo "## checking for requirements"
 cmds="lvcreate lvconvert gunzip virsh virt-edit"
 
 for i in $cmds
@@ -26,6 +35,12 @@ do
 	fi
 done
 
+if [ -z "$just_upgrade" ] || [ "$just_updagrade" == "yes" ]; then
+	echo "[info] Update complete"
+	exit 0
+fi
+
+echo "## running configuration scripts for initial setup"
 . setup-general.sh
 . setup-thin-lvm.sh
 . setup-libvirt-net.sh
