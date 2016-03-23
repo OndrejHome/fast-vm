2d8992d1e625c453a2ced5dac407e3d1de7a5e3d
Ondrej Famera <ondrej@famera.cz>
2016-03-23 23:40:52 +0100

    FIX: fast-vm-helper dhcp_release off-by-one(argument name) error fixed release of the DHCP reservations should now work when deleting VM

--- fast-vm-helper.sh
+++ fast-vm-helper.sh
@@ -78,14 +78,14 @@ case "$action" in
 		chgrp libvirt "$arg1" >>$DEBUG_LOG 2>&1
 		;;
 	dhcp_release)
-		arg1=$(echo "$arg1"| egrep '^[0-9]+$')
-		arg2=$(echo "$arg2"| egrep '^[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}$')
-		if [ -z "$arg1" ] || [ -z "$arg2" ] || [ "$arg1" -lt 20 ] || [ "$arg1" -gt 220 ]; then
+		arg2=$(echo "$arg2"| egrep '^[0-9]+$')
+		arg3=$(echo "$arg3"| egrep '^[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}$')
+		if [ -z "$arg2" ] || [ -z "$arg3" ] || [ "$arg2" -lt 20 ] || [ "$arg2" -gt 220 ]; then
 			echo "[err] validation of VM number or mac address failed"
 			exit 1
 		fi
 
-		dhcp_release "$LIBVIRT_NETWORK" "192.168.$SUBNET_NUMBER.$arg1" "$arg2" >>$DEBUG_LOG 2>&1
+		dhcp_release "$LIBVIRT_NETWORK" "192.168.$SUBNET_NUMBER.$arg2" "$arg3" >>$DEBUG_LOG 2>&1
 		;;
 	*)
 		echo "[err] unknown action"
