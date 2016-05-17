1beb3dfb159c1a629be5d68019eba14b39f92f60
Ondrej Famera <ondrej@famera.cz>
2016-05-18 00:16:47 +0200

    FIX: check for the presence of dhcp_release also in the fast-vm-helper.sh script

--- fast-vm-helper.sh
+++ fast-vm-helper.sh
@@ -78,14 +78,20 @@ case "$action" in
 		chgrp libvirt "$arg1" >>$DEBUG_LOG 2>&1
 		;;
 	dhcp_release)
-		arg2=$(echo "$arg2"| egrep '^[0-9]+$')
-		arg3=$(echo "$arg3"| egrep '^[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}$')
-		if [ -z "$arg2" ] || [ -z "$arg3" ] || [ "$arg2" -lt 20 ] || [ "$arg2" -gt 220 ]; then
-			echo "[err] validation of VM number or mac address failed"
-			exit 1
+		PATH="$PATH:/usr/sbin" which dhcp_release >/dev/null 2>&1
+		if [ "$?" -eq '0' ]; then
+			arg2=$(echo "$arg2"| egrep '^[0-9]+$')
+			arg3=$(echo "$arg3"| egrep '^[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}:[a-f0-9]{2,2}$')
+			if [ -z "$arg2" ] || [ -z "$arg3" ] || [ "$arg2" -lt 20 ] || [ "$arg2" -gt 220 ]; then
+				echo "[err] validation of VM number or mac address failed"
+				exit 1
+			fi
+
+			dhcp_release "$LIBVIRT_NETWORK" "192.168.$SUBNET_NUMBER.$arg2" "$arg3" >>$DEBUG_LOG 2>&1
+		else
+			printf "[wrn] dhcp_release not found, to reuse the same VM number you would need to delete DHCP leases file for $LIBVIRT_NETWORK network and restart this network in the libvirt.\nhttp://lists.thekelleys.org.uk/pipermail/dnsmasq-discuss/2007q1/001094.html\n"
 		fi
 
-		dhcp_release "$LIBVIRT_NETWORK" "192.168.$SUBNET_NUMBER.$arg2" "$arg3" >>$DEBUG_LOG 2>&1
 		;;
 	*)
 		echo "[err] unknown action"
