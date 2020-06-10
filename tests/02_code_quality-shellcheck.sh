8967bef05595de7c7c286e143799380454655aed
Ondrej Famera <ondrej@famera.cz>
2020-06-10 22:53:24 +0900

    shellcheck refactoring c3Iq

--- tests/02_code_quality-shellcheck.sh
+++ tests/02_code_quality-shellcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "##### shellcheck #####"
-for i in configure-fast-vm fast-vm fast-vm-helper.sh fast-vm-image fast-vm-net-cleanup; do echo -n "$i: "; shellcheck $i|wc -l; done
+for i in configure-fast-vm fast-vm fast-vm-helper.sh fast-vm-image fast-vm-net-cleanup; do echo -n "$i: "; shellcheck -x $i|wc -l; done
