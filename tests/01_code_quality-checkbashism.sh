dfd4499b41d66b5c52e50d6b1154b3f36c09e0b7
Ondrej Famera <ondrej@famera.cz>
2020-06-18 14:18:21 +0900

    adjust tests

--- tests/01_code_quality-checkbashism.sh
+++ tests/01_code_quality-checkbashism.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "##### checkbashism #####"
-for i in fast-vm fast-vm-helper.sh fast-vm-image configure-fast-vm fast-vm-net-cleanup; do echo "## ==== $i"; checkbashisms $i; done
+for i in fast-vm fast-vm-helper.sh fast-vm-image configure-fast-vm fast-vm-net-cleanup; do
+  echo "## ==== $i"
+  checkbashisms $i
+done
